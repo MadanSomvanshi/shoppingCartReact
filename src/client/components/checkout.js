e1387053efdfca839bf26693e4a338dfc71fcf73
madan-webonise <madan.somvanshi@weboniselab.com>
2019-02-27 19:05:17 +0530

    checkout page created with basic calculations

--- src/client/components/checkout.js
+++ src/client/components/checkout.js
@@ -4,21 +4,33 @@ export default class CheckoutShopping extends React.Component {
   constructor(props) {
     super(props);
     this.getSelectedItems = this.getSelectedItems.bind(this);
+    this.calculateBill = this.calculateBill.bind(this);
   }
-  
+
   getSelectedItems() {
     const getItems = JSON.parse(JSON.stringify(localStorage));
-    
     let fetchItem = _.map(getItems, item => {
-      console.log('item', item);
-      return item
+      if(item != "INFO") {
+        item = JSON.parse(item)
+        this.calculateBill(item.productCost, item.count)
+      }
+
+      return <li className='d-flex checkoutProductsList' key={item.productId}>
+      <span>{item.productName}</span>
+      <span>{item.productCost}</span>
+      <span>{item.count}</span>
+      <span>{item.productCost * item.count}</span>
+      </li>
     });
-
-
-    return <div>{fetchItem}</div>
-    
-
+    return <ul>{fetchItem}</ul>
   }
+
+  calculateBill(cost, count) {
+    let singleItemBill = cost * count;
+    let bill = bill + singleItemBill;
+    return bill;
+  }
+
 
   render() {
     return (
@@ -26,6 +38,7 @@ export default class CheckoutShopping extends React.Component {
         <div>Payment Flow need to integrate</div>
         <div>
           {this.getSelectedItems()}
+          <span>Total Bill</span><span>{this.calculateBill()}</span>
         </div>
       </div>
     )
--- src/client/components/header.js
+++ src/client/components/header.js
@@ -6,7 +6,11 @@ import {Link} from 'react-router-dom';
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productCount: this.props.productCount
+    }
   }
+
   render() {
     return (
       <header>
